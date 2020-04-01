75d10bab01d0b355903eb48a5033678a2b4a20b0
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-01 01:25:03 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -5,9 +5,9 @@ def my_collect(collection)
   i = 0
   new_collection = []
   while i < collection.length
-    binding.pry
     new_collection.push(collection[i])
     i += 1
   end
   new_collection
 end
+binding.pry

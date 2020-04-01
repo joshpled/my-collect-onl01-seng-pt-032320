0c98d53a4768e132ce86cda322bdea91e24ea78a
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-01 01:35:47 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+# require 'pry'
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    # binding.pry
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end

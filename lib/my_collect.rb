508405467e8037a6b4a157e189b25de9513d6e6b
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-01 01:34:27 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
-require 'pry'
+# require 'pry'
 def my_collect(array)
   i = 0
   collection = []
   while i < array.length
-    binding.pry
+    # binding.pry
     collection.push(array[i])
     i += 1
   end

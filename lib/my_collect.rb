9c93258963c7ee7df337ee0cbaff87f37a15db76
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-01 01:02:12 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,12 @@ def my_collect(collection)
   i = 0
   new_collection = []
   while i < collection.length
+    binding.pry
     new_collection.push(collection[i])
     i += 1
   end
 end
+
+# my_collect(collection) do |name|
+#   name.split(" ").first
+# end
